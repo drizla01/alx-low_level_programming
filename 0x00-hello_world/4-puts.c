41fb62b7e0d5226ecb150ff90f731088182a2b19
awal umar <awalumar277@gmail.com>
2022-10-13 07:11:06 -0700

    using printf function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - prints the text "Programming is like a multilingual puzzle
+ * Return: 0 if executed properly, if not return nonzero
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
