1c2d91de4b80e2c05b53edd054c725a64f5fc597
awal umar <awalumar277@gmail.com>
2022-10-14 09:20:32 -0700

    single digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+		printf("%d", digit);
+		putchar('\n');
+	return (0);
+}
