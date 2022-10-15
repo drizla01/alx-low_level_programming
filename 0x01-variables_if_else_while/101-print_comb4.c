449b183ef6056a5aa260faa59b0633603ce47adb
awal umar <awalumar277@gmail.com>
2022-10-14 20:56:12 -0700

    combination of three numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - combination if three digit
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 48; a <= 55; a++)
+	{
+		for (b = 49; b <= 56; b++)
+		{
+			for (c = 50; c <= 57; c++)
+			{
+				if (c > b && b > a)
+				{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+					if (a != 55 || b != 56 || c != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
+				}
+
+			}
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
