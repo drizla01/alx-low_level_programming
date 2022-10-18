cbc3649e70dc9e0c885c432de554bf10b059f926
awal umar <awalumar277@gmail.com>
2022-10-18 16:27:14 -0700

    putting characters to gether using putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,22 @@
 #include "main.h"
 
 /**
- * main - the body of the function
- * Return: 0 if succcessful
+ * main - prints the characters
+ * Return: 0 if successful
+ *
  */
 
 int main(void)
 {
-	_putchar("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
