1adcea7b626e4aefcf98ebe10c48030cdb75e444
awal umar <awalumar277@gmail.com>
2022-10-18 03:31:54 -0700

    putchar output

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
  * main - the body of the function
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar("_putchar\n");
 	return (0);
 }
