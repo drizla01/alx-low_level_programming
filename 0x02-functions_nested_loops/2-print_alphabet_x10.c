3b329dea8a2209bc48c30eb4a075748cd7594cf1
awal umar <awalumar277@gmail.com>
2022-10-19 08:59:12 -0700

    printing letter times 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabets a to z,
+ * ten times in lower case
+ * Return: nothing if successful
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+
+		char alp;
+
+		for (alp = 'a'; alp <= 'z'; alp++)
+		{
+			_putchar(alp);
+		}
+
+			_putchar('\n');
+
+			i++;
+	}
+}
