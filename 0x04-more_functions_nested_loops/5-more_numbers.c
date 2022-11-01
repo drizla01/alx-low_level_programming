e87a389cdadab248f6efbcd0ad05cbfb88eb95ef
awal umar <awalumar277@gmail.com>
2022-11-01 10:55:14 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints number from o to 14 10times
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int i, num;
+
+	i = 0;
+	while (i < 10)
+	{
+		for ( num = 0; num < 15; num++)
+		{
+			if ( num > 9)
+			{
+				_putchar((num / 10) + '0');
+			}
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+
+		i++;
+	}
+}
