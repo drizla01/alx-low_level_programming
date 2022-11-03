162e943829eacbf0b1980c46170abf7c0ad956d1
awal umar <awalumar277@gmail.com>
2022-11-03 04:08:11 -0700

    update square root recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,21 +1,36 @@
 #include "main.h"
 
+int find_srt(int num, int root);
+int _sqrt_recursion(int n);
+
+/**
+ * find_srt - return the square root
+ * @root: the root of the number
+ * @num: the inputted number
+ * Return: square root if the number is a perfect square,
+ * otherwise return -1
+ */
+int find_srt(int num, int root)
+{
+	if ((root * root) == num)
+	{
+		return (root);
+	}
+	else if ((root * root) < num || (root * root) > num)
+	{
+		return (-1);
+	}
+
+	return (find_srt(num, (root + 1)));
+}
+
 /**
  * _sqrt_recursion - return the natural square root of number
  * @n: the number to evaluate
+ * find_srt - return the square root
  * Return: the square root of n, if
  * n does not have natural sqrt return -1.
  */
-int find_srt(int num, int sqrt)
-{
-	if ((root * root) == num);
-		return (root);
-	else if (root == num / 2)
-		return (-1);
-	return (find_srt(num, root + 1))
-}
-
-
 
 int _sqrt_recursive(int n)
 {
