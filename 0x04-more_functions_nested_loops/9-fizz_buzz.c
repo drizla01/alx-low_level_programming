423bb6d2efefb03ae32bc0ba0cfc781cfeff5ee5
awal umar <awalumar277@gmail.com>
2022-11-06 08:09:06 -0800

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - print a fizzbuzz program
+ * Return: if the number is divisible by 3 prìnt Fizz
+ * if it is divisible by 5, print Buzz
+ * if it is divisible by both 3 and 5 print FizzBuzz
+ * otherwise, print the number
+ */
+int main(void)
+{
+	int nb;
+
+	for (nb = 1; nb <= 100; nb++)
+	{
+		if ((nb % 3) == 0 && (nb % 5) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if ((nb % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((nb % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", nb);
+		}
+		if (nb == 100)
+			continue;
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
