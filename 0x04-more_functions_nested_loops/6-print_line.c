4b617bf2a2612897fb0e4ba82ac55a381a613c24
awal umar <awalumar277@gmail.com>
2022-11-06 06:41:12 -0800

    new line printing

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - return the length of a line
+ * @n: variable to store the value
+ * Return: the line
+ */
+void print_line(int n)
+{
+	int len;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+			_putchar('-');
+
+	}
+	_putchar('\n');
+}
+
