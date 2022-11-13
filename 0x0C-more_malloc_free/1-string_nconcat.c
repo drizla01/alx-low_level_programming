800707f833c4b9031518a013ee6d7af7c9231898
awal umar <awalumar277@gmail.com>
2022-11-13 13:54:31 -0800

    string concat update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,9 +19,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len = n, index;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
 		s2 = "";
 	for (index = 0; s1[index]; index++)
@@ -42,4 +40,3 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	concat[len] = '\0';
 	return (concat);
 }
-
