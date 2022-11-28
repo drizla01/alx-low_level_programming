a981a54ceec1726ad5d5a99b24ceec93755536c9
awal umar <awalumar277@gmail.com>
2022-11-28 06:18:10 -0800

    betty still complains about splitting the line in printf

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * File: 100-first.c
+ * Auth: Brennan D. Baraban
+ */
+
+
+void __attribute__((constructor))hare(void);
+
+/**
+ * hare - prints a string before the main function is executed
+ */
+
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
