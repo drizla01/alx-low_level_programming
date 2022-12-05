c0c516db715f41e0dcba9e172fe71241f62633ec
awal umar <awalumar277@gmail.com>
2022-12-05 08:18:10 -0800

    updated read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,14 +1,15 @@
 #include "main.h"
-#include <stddef.h>
+#include <stdlib.h>
 
 /**
- * read_textfile - read a text file and prints it to the standard output.
- * @filename: name of the file
- * @letters: number of letters to be printed
+ * read_textfile - read the text file and prints it to POSIX output
+ * @filename: the name of the file
+ * @letters: the letters of the file
  *
- * Return: number of letters read and printed
+ * Return: the letter of the file that we can read and print,
+ * if file cannot be open or read return 0, if filename is NULL return 0.
+ * if write fails or does not write the expected amount of bytes, return 0.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file, n_read, wrote;
@@ -21,9 +22,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		return (0);
+	}
+	n_read = read(file, buffer, letters);
 	if (n_read == -1)
 		return (0);
-
+	buffer[n_read] = '\0';
+	wrote = write(STDOOUT_FILENO, buffer, n_read);
+	if (wrote != n_read)
+		return (0);
 	free(buffer);
 	close(file);
 	return (n_read);
