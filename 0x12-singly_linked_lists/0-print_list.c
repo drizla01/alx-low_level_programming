311a42866787eaf50911d72c41461dc951aa32f1
Awal Umar <awalumar277@gmail.com>
2023-09-07 06:49:31 -0700

    header

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,6 +3,22 @@
 
 size_t print_list(const list_t *h):
 {
-	sie_t num = 0;
+	size_t num = 0;
+	list_t *p = h ? h->next : NULL;
 
+	if (h)
+	{
+		printf("[%d] %s\n", h->len : 0, h->str : "(nil)");
+		num++;
+	}
+	if (p)
+	{
+		while (p)
+		{
+		printf("[%d] %s\n", p->str ? p->len : 0, p->str ? p->str : "(nil)");
+		num++;
+		p = p->next;
+		}
+	}
+	return (num)
 }
